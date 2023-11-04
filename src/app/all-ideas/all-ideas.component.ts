1fcc96361ae37946c9c61313ee38088cf565b173
MariaGalGon <margalgon@hotmail.es>
2023-11-04 13:48:21 +0100

    Project with Material, all ideas component and UPM logo

---
+++ src/app/all-ideas/all-ideas.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-all-ideas',
+  templateUrl: './all-ideas.component.html',
+  styleUrls: ['./all-ideas.component.scss']
+})
+export class AllIdeasComponent {
+
+}
