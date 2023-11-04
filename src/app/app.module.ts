1ff134c10747b5a90141bb45544b49bade006315
Adrián Sánchez Rodero <adrian.sanchez.rodero@alumnos.upm.es>
2023-11-04 19:47:55 +0100

    Deleted all ideas view and created home view.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AllIdeasComponent } from './all-ideas/all-ideas.component';
+import { HomeComponent } from './views/home/home.component';
+import { MyIdeasComponent } from './views/my-ideas/my-ideas.component';
+import { LoginFormComponent } from './components/login-form/login-form.component';
+import { RegisterFormComponent } from './components/register-form/register-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AllIdeasComponent
+    HomeComponent,
+    MyIdeasComponent,
+    LoginFormComponent,
+    RegisterFormComponent,
   ],
   imports: [
     BrowserModule,
