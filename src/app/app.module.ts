8ba054999f487430416931811886f6aade48689a
Adrián Sánchez Rodero <adrian.sanchez.rodero@alumnos.upm.es>
2023-11-04 20:34:09 +0100

    Default actions on save.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HomeComponent } from './views/home/home.component';
 import { LoginFormComponent } from './components/login-form/login-form.component';
 import { RegisterFormComponent } from './components/register-form/register-form.component';
+import { HomeComponent } from './views/home/home.component';
 
 @NgModule({
   declarations: [
