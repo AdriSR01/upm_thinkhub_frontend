82ce6fea7fe9d7ad8e8befb1f069ecd446e90fad
Guillermo-prog <72439217+Guillermo-prog@users.noreply.github.com>
2023-11-07 18:49:23 +0100

    Implement the list of ideas in the dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { ReactiveFormsModule } from '@angular/forms';
-import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -12,6 +14,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IdeasListComponent } from './components/ideas-list/ideas-list.component';
 import { LoginFormComponent } from './components/login-form/login-form.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { RegisterFormComponent } from './components/register-form/register-form.component';
@@ -23,11 +26,14 @@ import { HomeComponent } from './views/home/home.component';
     HomeComponent,
     LoginFormComponent,
     RegisterFormComponent,
-    NavBarComponent
+    NavBarComponent,
+    IdeasListComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    MatCardModule,
+    MatButtonModule,
     BrowserAnimationsModule,
     MatFormFieldModule,
     MatInputModule,
@@ -37,8 +43,9 @@ import { HomeComponent } from './views/home/home.component';
     MatToolbarModule,
     MatDividerModule,
     MatDialogModule,
+    MatListModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/components/ideas-list/ideas-list.component.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+import { Idea } from 'src/app/core/models/Idea';
+
+@Component({
+  selector: 'app-ideas-list',
+  templateUrl: './ideas-list.component.html',
+  styleUrls: ['./ideas-list.component.scss'],
+})
+export class IdeasListComponent {
+  ideas: Idea[] = [
+    {
+      title: 'Smart Home Energy Management embedded system',
+      description:
+        'An intelligent system that optimizes energy usage in homes, reducing electricity bills and environmental impact. This innovative technology utilizes advanced sensors and machine learning algorithms to monitor and control various appliances and lighting, ensuring optimal energy efficiency. By dynamically adjusting power consumption based on user preferences and real-time data, this system not only provides significant cost savings but also contributes to a greener and more sustainable future.',
+      likes: 325,
+      user_id: 'user1',
+      id: 'idea1',
+      topics: [
+        'Sustainability',
+        'Technology',
+        'Energy Efficiency',
+        'Technology',
+      ],
+    },
+    {
+      title: 'AI-Powered Healthcare Assistant',
+      description:
+        'A virtual healthcare assistant that uses AI to provide personalized medical advice and monitor patient health. This cutting-edge technology leverages artificial intelligence to deliver tailor-made health recommendations based on an individuals medical history, lifestyle, and specific health goals. Additionally, it continuously tracks and assesses the patients vital signs, ensuring timely intervention in case of any health anomalies. With its user-friendly interface and round-the-clock availability, this virtual healthcare assistant empowers individuals to take proactive control of their well-being and make informed decisions about their health.',
+      likes: 478,
+      user_id: 'user2',
+      id: 'idea2',
+      topics: ['Technology', 'Home Automation'],
+    },
+    {
+      title: 'Urban Vertical Farming',
+      description:
+        'Revolutionizing urban agriculture by growing crops in vertical, high-tech farms to address food security and sustainability. This innovative approach to farming involves the utilization of advanced vertical farming techniques and state-of-the-art technology to cultivate a wide variety of crops in densely populated urban areas. By optimizing space and resources, these vertical farms are not only enhancing food security but also contributing to a more sustainable and environmentally friendly food production system. With a focus on locally sourced, fresh, and pesticide-free produce, this initiative promises to reshape the way we grow and access food in urban environments, promoting healthier diets and reducing the carbon footprint associated with traditional agriculture.',
+      likes: 212,
+      user_id: 'user3',
+      id: 'idea3',
+      topics: ['Renewable Energy', 'Technology', 'Home Automation'],
+    },
+  ];
+}
