0d14b0cd258f452307cdab6e376c2f66d7492aad
MariaGalGon <margalgon@hotmail.es>
2023-11-07 22:47:17 +0100

    Elementos de formulario de edicion de una idea

---
+++ src/app/views/edit-idea/edit-idea.component.ts
@@ -0,0 +1,47 @@
+import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-edit-idea',
+  templateUrl: './edit-idea.component.html',
+  styleUrls: ['./edit-idea.component.scss'],
+})
+export class EditIdeaComponent {
+  form: FormGroup;
+  isNewIdea: boolean;
+  topics: string[] = [
+    'Technological',
+    'Industry',
+    'Commerce',
+    'Environment',
+    'Social',
+  ];
+
+  constructor(private formBuilder: FormBuilder, private router: Router) {
+    this.form = this.formBuilder.group({
+      title: new FormControl('', [Validators.required]),
+      description: new FormControl('', [Validators.required]),
+      topic: new FormControl('', [Validators.required]),
+    });
+
+    this.isNewIdea = this.router.url === 'publish-idea';
+  }
+
+  isFormValid() {
+    return (
+      !this.form.controls['title'].invalid &&
+      !this.form.controls['description'].invalid &&
+      !this.form.controls['topic'].invalid
+    );
+  }
+
+  publishIdea() {}
+
+  saveIdea() {}
+}
