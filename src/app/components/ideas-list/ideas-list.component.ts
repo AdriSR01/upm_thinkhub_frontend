be233536aef21d37181cb38d545cfea48811a1f7
MariaGalGon <margalgon@hotmail.es>
2023-11-11 18:25:03 +0100

    Sort idea by likes buttons

--- src/app/components/ideas-list/ideas-list.component.ts
+++ src/app/components/ideas-list/ideas-list.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { Idea } from '../../core/models/Idea';
-import {Router} from "@angular/router";
-import {AuthService} from "../../core/services/auth.service";
-import {IdeasService} from "../../core/services/backend/ideas.service";
+import { AuthService } from '../../core/services/auth.service';
+import { IdeasService } from '../../core/services/backend/ideas.service';
 
 @Component({
   selector: 'app-ideas-list',
@@ -14,10 +14,14 @@ export class IdeasListComponent {
 
   showPublish = false;
   loading = false;
+  ideasAscending = false;
+  ideasDescending = false;
 
-  constructor(private router: Router,
-              private authService: AuthService,
-              private ideasService: IdeasService) {
+  constructor(
+    private router: Router,
+    private authService: AuthService,
+    private ideasService: IdeasService
+  ) {
     this.showPublish = this.authService.user !== undefined;
     this.authService.loggedEvent.subscribe(() => {
       this.showPublish = this.authService.user !== undefined;
@@ -25,17 +29,29 @@ export class IdeasListComponent {
 
     this.loading = true;
     this.ideasService.getAllIdeas().subscribe({
-      next: (ideas:Idea[]) => {
+      next: (ideas: Idea[]) => {
         this.ideas = ideas;
         this.loading = false;
       },
       error: (error) => {
         console.log(error);
-      }
-    })
+      },
+    });
   }
 
-  publishIdea(){
+  publishIdea() {
     this.router.navigate(['publishIdea']);
+  }
+
+  sortAscending() {
+    this.ideasAscending = !this.ideasAscending;
+    this.ideasDescending = false;
+    // TODO: Sort ideas by ascending order
+  }
+
+  sortDescending() {
+    this.ideasDescending = !this.ideasDescending;
+    this.ideasAscending = false;
+    // TODO: Sort ideas by descending order
   }
 }
