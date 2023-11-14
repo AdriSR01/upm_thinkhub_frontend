11fc74178f68e9af317b2c8adbd580d89591ce52
Adrián Sánchez Rodero <adrian.sanchezrodero@vipera.com>
2023-11-14 11:38:17 +0100

    Added route for details idea view and navigate from idea item.

--- src/app/components/idea-item/idea-item.component.ts
+++ src/app/components/idea-item/idea-item.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { Idea } from 'src/app/core/models/Idea';
 import {IdeasService} from "../../core/services/backend/ideas.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-idea-item',
@@ -11,7 +12,8 @@ export class IdeaItemComponent {
   @Input() idea!: Idea;
 
   allowLike = true;
-  constructor(private ideasService: IdeasService) {
+  constructor(private ideasService: IdeasService,
+              private router: Router) {
   }
   addLike() {
     this.allowLike = false;
@@ -23,5 +25,9 @@ export class IdeaItemComponent {
         console.log(error);
       }
     })
+  }
+
+  goToDetail() {
+    this.router.navigate([`detailsIdea/${this.idea.id}`]);
   }
 }
