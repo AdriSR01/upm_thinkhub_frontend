a7579945a9b9c0f0f73f31446036691e8e17b52b
Adrián Sánchez Rodero <adrian.sanchezrodero@vipera.com>
2023-11-14 11:57:03 +0100

    Added input to control show detail button in idea item.

--- src/app/components/idea-item/idea-item.component.ts
+++ src/app/components/idea-item/idea-item.component.ts
@@ -1,5 +1,5 @@
-import { Component, Input } from '@angular/core';
-import { Idea } from 'src/app/core/models/Idea';
+import {Component, Input} from '@angular/core';
+import {Idea} from 'src/app/core/models/Idea';
 import {IdeasService} from "../../core/services/backend/ideas.service";
 import {Router} from "@angular/router";
 
@@ -10,15 +10,18 @@ import {Router} from "@angular/router";
 })
 export class IdeaItemComponent {
   @Input() idea!: Idea;
+  @Input() showDetailButton = true;
 
   allowLike = true;
+
   constructor(private ideasService: IdeasService,
               private router: Router) {
   }
+
   addLike() {
     this.allowLike = false;
     this.ideasService.addLike(this.idea.id!).subscribe({
-      next: (idea: Idea)=> {
+      next: (idea: Idea) => {
         this.idea = idea;
       },
       error: (error) => {
