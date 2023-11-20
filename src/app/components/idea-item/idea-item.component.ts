38aeb5aaa3bdc20239538c268b9407e2deec916a
Adrián Sánchez Rodero <adrian.sanchez.rodero@alumnos.upm.es>
2023-11-20 21:35:50 +0100

    Rename variable.

--- src/app/components/idea-item/idea-item.component.ts
+++ src/app/components/idea-item/idea-item.component.ts
@@ -10,7 +10,7 @@ import {Router} from "@angular/router";
 })
 export class IdeaItemComponent {
   @Input() idea!: Idea;
-  @Input() showDetailButton = true;
+  @Input() isNotDetail = true;
 
   allowLike = true;
 
