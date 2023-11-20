3e562b244f0e9fa7b5eba4e1003f99831d263353
Adrián Sánchez Rodero <adrian.sanchez.rodero@alumnos.upm.es>
2023-11-20 21:42:40 +0100

    Added return type.

--- src/app/core/services/backend/comments.service.ts
+++ src/app/core/services/backend/comments.service.ts
@@ -1,16 +1,15 @@
-import { HttpResponse } from "@angular/common/http";
-import { Injectable } from "@angular/core";
-import { Observable } from "rxjs";
-import { Comment } from "../../models/Comment";
-import { ApiService } from "./api.service";
+import {Injectable} from "@angular/core";
+import {Observable} from "rxjs";
+import {Comment} from "../../models/Comment";
+import {ApiService} from "./api.service";
 
 @Injectable({
   providedIn: 'root',
 })
-export class CommentsService extends ApiService{
+export class CommentsService extends ApiService {
   readonly SERVICE_NAME = 'feedback/comment';
 
-  createComment(comment: Comment, ideaId: string): Observable<HttpResponse<any>>{
-    return this.post(`${this.SERVICE_NAME}/${ideaId}`, comment);
+  createComment(comment: Comment, ideaId: string): Observable<Comment> {
+    return this.post<Comment>(`${this.SERVICE_NAME}/${ideaId}`, comment);
   }
 }
