ed370d140868470ab48e4bc6d69823999f98f33e
Adrián Sánchez Rodero <adrian.sanchez.rodero@alumnos.upm.es>
2023-11-21 17:53:24 +0100

    Used likes service to allow or not allow like.

--- src/app/components/idea-item/idea-item.component.ts
+++ src/app/components/idea-item/idea-item.component.ts
@@ -1,21 +1,27 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {Idea} from 'src/app/core/models/Idea';
 import {IdeasService} from "../../core/services/backend/ideas.service";
 import {Router} from "@angular/router";
+import {LikesService} from "../../core/services/likes.service";
 
 @Component({
   selector: 'app-idea-item',
   templateUrl: './idea-item.component.html',
   styleUrls: ['./idea-item.component.scss'],
 })
-export class IdeaItemComponent {
+export class IdeaItemComponent implements OnInit {
   @Input() idea!: Idea;
   @Input() isNotDetail = true;
 
   allowLike = true;
 
   constructor(private ideasService: IdeasService,
+              private likesService: LikesService,
               private router: Router) {
+  }
+
+  ngOnInit() {
+    this.allowLike = !this.likesService.hasLike(this.idea.id!);
   }
 
   addLike() {
@@ -23,6 +29,7 @@ export class IdeaItemComponent {
     this.ideasService.addLike(this.idea.id!).subscribe({
       next: (idea: Idea) => {
         this.idea = idea;
+        this.likesService.addLike(idea.id!);
       },
       error: (error) => {
         console.log(error);
